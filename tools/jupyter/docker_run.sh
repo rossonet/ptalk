c5bce2760cdc25d1cc545b50e401798eedbacf8a
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2023-02-20 00:25:31 +0100

    varie

--- tools/jupyter/docker_run.sh
+++ tools/jupyter/docker_run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -p 8888:8888 --rm -v $(pwd)/data:/home/jovyan/work jupyter/scipy-notebook
+mkdir -p data
+docker run -p 8888:8888 --name jupyter --rm -v $(pwd)/data:/home/jovyan/work \
+       rossonet/jupyter-lab:latest 
+       #jupyter/scipy-notebook
