0de0a70572e9e4caafcfdecccdcce185f42a8e75
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2023-02-20 18:58:33 +0100

    varie

--- tools/jupyter/docker_build.sh
+++ tools/jupyter/docker_build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build --no-cache -t rossonet/jupyter-lab:latest .
+#docker build --no-cache -t rossonet/jupyter-lab:latest .
+docker build -t rossonet/jupyter-lab:latest .
---
+++ tools/jupyter/examples/channel/README.md
@@ -0,0 +1,1 @@
+[GRPC Python reference](https://grpc.io/docs/languages/python/)
